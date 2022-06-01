c9314060b77a4315f4734fcde9278a2f89117039
korean_kat <katarina999@outlook.fr>
2022-06-01 13:05:24 +0100

    v1.1(emailjs update)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,108 +1,104 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
-import React, { useRef } from 'react';
-import './contact.scss';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import emailjs from 'emailjs-com';
-import {
-  NotificationContainer,
-  NotificationManager,
-} from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
+import React, { useRef } from "react"
+import "./contact.scss"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import emailjs from "emailjs-com"
+import { NotificationContainer, NotificationManager } from "react-notifications"
+import "react-notifications/lib/notifications.css"
 
-
-AOS.init();
+AOS.init()
 
 function Contact() {
-  const form = useRef();
+  const form = useRef()
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     emailjs
       .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
+        "service_k5douhi",
+        "template_xgoeijp",
         form.current,
-        process.env.REACT_APP_USER_ID
+        "4AFUO7Ua2XiLUj_hH"
       )
       .then(
         (result) => {
-          console.log(result.text);
+          console.log(result.text)
           NotificationManager.success(
-            'Thank you for contacting me',
-            'Message sent!'
-          );
+            "Thank you for contacting me",
+            "Message sent!"
+          )
         },
         (error) => {
-          console.log(error.text);
+          console.log(error.text)
           NotificationManager.error(
-            'Please try again later',
-            'Message could not be sent!'
-          );
+            "Please try again later",
+            "Message could not be sent!"
+          )
         }
-      );
-    e.target.reset();
-  };
+      )
+    e.target.reset()
+  }
 
   return (
-    <div className="Contact" id="Contact">
-      <h1 className="section-header">Contact</h1>
-      <div className="contact-form">
+    <div className='Contact' id='Contact'>
+      <h1 className='section-header'>Contact</h1>
+      <div className='contact-form'>
         <form
-          role="form"
+          role='form'
           onSubmit={sendEmail}
           ref={form}
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="900"
-          data-aos-easing="ease"
-          data-aos-mirror="false"
-          data-aos-once="true"
+          data-aos='fade-up'
+          data-aos-offset='200'
+          data-aos-delay='50'
+          data-aos-duration='900'
+          data-aos-easing='ease'
+          data-aos-mirror='false'
+          data-aos-once='true'
         >
-          <div className="row-1">
+          <div className='row-1'>
             <input
-              type="text"
-              autoComplete="off"
-              id="name"
-              name="name"
-              placeholder="Name"
-              spellCheck="false"
+              type='text'
+              autoComplete='off'
+              id='name'
+              name='name'
+              placeholder='Name'
+              spellCheck='false'
               required
             ></input>
 
             <input
-              type="email"
-              autoComplete="off"
-              id="email"
-              name="email"
-              placeholder="Email"
-              spellCheck="false"
+              type='email'
+              autoComplete='off'
+              id='email'
+              name='email'
+              placeholder='Email'
+              spellCheck='false'
               required
             ></input>
           </div>
 
           <input
-            type="text"
-            autoComplete="off"
-            id="subject"
-            name="subject"
-            placeholder="Subject"
-            spellCheck="false"
+            type='text'
+            autoComplete='off'
+            id='subject'
+            name='subject'
+            placeholder='Subject'
+            spellCheck='false'
           ></input>
           <br></br>
 
           <textarea
-            id="message"
-            autoComplete="off"
-            name="message"
-            placeholder="Message"
-            spellCheck="false"
+            id='message'
+            autoComplete='off'
+            name='message'
+            placeholder='Message'
+            spellCheck='false'
           ></textarea>
           <br></br>
-          <div className="form-button">
-            <button type="submit" value="Submit" class="slide">
+          <div className='form-button'>
+            <button type='submit' value='Submit' class='slide'>
               Send Message!
             </button>
           </div>
@@ -110,7 +106,7 @@ function Contact() {
       </div>
       <NotificationContainer />
     </div>
-  );
+  )
 }
 
-export default Contact;
+export default Contact
